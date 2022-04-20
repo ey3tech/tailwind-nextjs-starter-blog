776475f853f77266fe89b9342e64fb4e44cc0b82
thrizzle <thrzl@terabyteis.me>
2022-04-20 17:57:24 +0000

    🐛 make it so that these icons are optional

--- layouts/AuthorLayout.js
+++ layouts/AuthorLayout.js
@@ -1,13 +1,16 @@
 import SocialIcon from '@/components/social-icons'
 import Image from '@/components/Image'
 
-export default function AuthorLayout({ children, frontMatter }) {
+export default function AuthorLayout({ children, frontMatter, key }) {
   const { name, avatar, occupation, company, email, twitter, linkedin, github, website } =
     frontMatter
 
   return (
     <>
-      <div className="items-start space-y-2 xl:grid p-10 xl:grid-cols-3 xl:gap-x-8 xl:space-y-0 dark:bg-gray-800 bg-gray-100 rounded-xl">
+      <div
+        key={key}
+        className="items-start space-y-2 rounded-xl bg-gray-100 p-10 dark:bg-gray-800 xl:grid xl:grid-cols-3 xl:gap-x-8 xl:space-y-0"
+      >
         <div className="flex flex-col items-center pt-8">
           <Image
             src={avatar}
@@ -22,10 +25,11 @@ export default function AuthorLayout({ children, frontMatter }) {
           <div className="text-gray-500 dark:text-gray-400">{occupation}</div>
           <div className="text-gray-500 dark:text-gray-400">{company}</div>
           <div className="flex space-x-3 pt-6">
-            <SocialIcon kind="mail" href={`mailto:${email}`} />
-            {/* <SocialIcon kind="github" href={github} /> */}
-            <SocialIcon kind="linkedin" href={linkedin} />
-            <SocialIcon kind="twitter" href={twitter} />
+            {email ? <SocialIcon kind="mail" href={`mailto:${email}`} /> : <></>}
+            {github ? <SocialIcon kind="github" href={github} /> : <></>}
+            {linkedin ? <SocialIcon kind="linkedin" href={linkedin} /> : <></>}
+            {twitter ? <SocialIcon kind="twitter" href={twitter} /> : <></>}
+            {website ? <SocialIcon kind="globe" href={website} /> : <></>}
           </div>
         </div>
         <div className="prose max-w-none pt-8 pb-8 dark:prose-dark xl:col-span-2">{children}</div>
