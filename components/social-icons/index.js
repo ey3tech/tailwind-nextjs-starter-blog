a20f3a3e6ea05717bd560cc1e2bfc20fbadea794
thrizzle <thrzl@terabyteis.me>
2022-04-20 17:56:33 +0000

    🍱 svg fix + add globe

--- components/social-icons/index.js
+++ components/social-icons/index.js
@@ -4,8 +4,9 @@ import Facebook from './facebook.svg'
 import Youtube from './youtube.svg'
 import Linkedin from './linkedin.svg'
 import Twitter from './twitter.svg'
+import Globe from './globe.svg'
 
-// Icons taken from: https://simpleicons.org/
+// Icons taken from: https://simpleicons.org/ + feather icons
 
 const components = {
   mail: Mail,
@@ -14,6 +15,7 @@ const components = {
   youtube: Youtube,
   linkedin: Linkedin,
   twitter: Twitter,
+  globe: Globe,
 }
 
 const SocialIcon = ({ kind, href, size = 8 }) => {
@@ -31,7 +33,7 @@ const SocialIcon = ({ kind, href, size = 8 }) => {
     >
       <span className="sr-only">{kind}</span>
       <SocialSvg
-        className={`fill-current text-gray-700 hover:text-blue-500 dark:text-gray-200 dark:hover:text-blue-400 h-${size} w-${size}`}
+        className={`text-gray-700 hover:text-blue-500 dark:text-gray-200 dark:hover:text-blue-400 h-${size} w-${size}`}
       />
     </a>
   )
